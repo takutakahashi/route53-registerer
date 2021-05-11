4dee40fbb137b39203d409d7a974cc0910fe390b
takutaka <takutaka@pepabo.com>
2021-05-11 14:59:42 +0900

    enable csv batch

--- lib/parse_j2.py
+++ lib/parse_j2.py
@@ -4,9 +4,9 @@ import os
  
 env = Environment(loader=FileSystemLoader('.'))
 template = env.get_template(sys.argv[1])
- 
+records = os.environ.get("RECORD_NAME").split(",")
 data = {
-        "record": os.environ.get("RECORD_NAME"),
+        "records": records,
         "type": os.environ.get("TYPE"),
         "val": os.environ.get("VAL"),
         "hosted_zone_id": os.environ.get("HOSTED_ZONE_ID")
