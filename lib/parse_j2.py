6a81166344ca0e821000d47a993441423b01246b
takutaka <takutaka@pepabo.com>
2021-05-12 15:26:33 +0900

    add base_domain

--- lib/parse_j2.py
+++ lib/parse_j2.py
@@ -5,6 +5,9 @@ import os
 env = Environment(loader=FileSystemLoader('.'))
 template = env.get_template(sys.argv[1])
 records = os.environ.get("RECORD_NAME").split(",")
+domain = os.environ.get("BASE_DOMAIN")
+if domain != None:
+        records = list(map(lambda x: x+'.'+domain, records))
 data = {
         "records": records,
         "type": os.environ.get("TYPE"),
